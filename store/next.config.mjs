694fb2fb8dc3d5beb92a5de22885f63e9d4341b8
pixlane <pixlane@outlook.com>
2026-03-07 21:07:38 +0300

    Update next.config.mjs

--- store/next.config.mjs
+++ store/next.config.mjs
@@ -1,9 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "plus.unsplash.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
     ],
   },
 };
